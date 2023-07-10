06bf625635234775164c8c451c0980ccc7a1d2d9
Expertz <xeniapliner@gmail.com>
2023-07-10 18:51:18 +0300

    Sem Task 24

---
+++ Sem4_Show_HW_polindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿//Решение ДЗ по решению полиндром или нет с помощью разворота числа 
+//(КАК ПЕРЕВЕРНУТЬ ЧИСЛО)
+
+//48697 == polindrom?321`
+
+bool IsPalindrome (int usernumber)
+{
+    {
+        if (usernumber < 0 )
+        return false;
+    }
+
+int revNumber = 0;
+int TempMethodNumber = usernumber;
+while (TempMethodNumber !=0)
+{
+    // Крайняя правая цифра исходного числа
+    int temp = TempMethodNumber % 10;
+    //Формируем новое число 
+    revNumber = (revNumber * 10) + temp;
+
+    //Уменьшение исходного числа
+    TempMethodNumber = TempMethodNumber / 10;
+} 
+if (revNumber == usernumber)
+    return true;
+
+return false;
+}
+
+Console.WriteLine(IsPalindrome(222221));
+
